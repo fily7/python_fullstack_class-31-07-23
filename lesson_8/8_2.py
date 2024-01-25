a3024a640e3f3d5a4ee35c1cf6dd4c099f0e4b3e
Igor Potashev <fil_igor@bk.ru>
2024-01-25 14:31:19 +0300

    lesson_8 rework

--- lesson_8/8_2.py
+++ lesson_8/8_2.py
@@ -4,9 +4,12 @@ words = {
     "Сложный": "Запутанный",
     "Простой": "Легкий",
 }
-for key, value in words.items():
+for key, value in list(words.items()):
     words[value] = key
 
+
+assert words["Красивый"] == "Прекрасный"
+assert words["Прекрасный"] == "Красивый"
 
 key = input("Введите слово: ")
 print(f"Синоним: {words[key]}")
--- lesson_8/8_5.py
+++ lesson_8/8_5.py
@@ -1,12 +1,28 @@
-first_shop_goods = set((input("Первый магазин: ").split(", ")))
-second_shop_goods = set((input("Второй магазин: ").split(", ")))
+from typing import Tuple
 
 
-print(
-    "Только в первом магазине:",
-    ", ".join(first_shop_goods.difference(second_shop_goods)),
-)
-print(
-    "Только в втором магазине:",
-    ", ".join(second_shop_goods.difference(first_shop_goods)),
-)
+def cross_dif(a: set, b: set) -> Tuple[set, set]:
+    return a - b, b - a
+
+
+def main():
+    first_shop_goods = set((input("Первый магазин: ").split(", ")))
+    second_shop_goods = set((input("Второй магазин: ").split(", ")))
+    uniq_first, uniq_second = cross_dif(first_shop_goods, second_shop_goods)
+    print("Только в первом магазине:", ", ".join(uniq_first))
+    print("")
+    print("Только в втором магазине:", ", ".join(uniq_second))
+
+
+if __name__ == "__main__":
+    assertion = True
+
+    if assertion:
+        a = set(["a", "b", "c", "d"])
+        b = set(["c", "d", "e", "f"])
+        ua, ub = cross_dif(a, b)
+        assert ua == set(["a", "b"])
+        assert ub == set(["e", "f"])
+        print("test: OK")
+    else:
+        main()
